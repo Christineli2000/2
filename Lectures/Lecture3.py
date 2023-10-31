eb49b81e9265c2de918dde1c4a2a2715bd5f0727
Leyan Li <Christineli2000@outlook.com>
2023-10-31 13:40:36 +1100

    Initial Commit

---
+++ Lectures/Lecture3.py
@@ -0,0 +1,10 @@
+a = 0
+b = True
+if a != 0:
+   print("a is non-zero")
+elif b is True:
+   print("b is True")
+elif a < 0 and b is True:
+   print("a is negative and b is True")
+else:
+   print("None of the conditions above hold")
