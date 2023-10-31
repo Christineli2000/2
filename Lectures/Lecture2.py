0a8140969559deb3054a10e1a8d58084ac9a4c3d
Leyan Li <Christineli2000@outlook.com>
2023-10-31 13:40:01 +1100

    Initial Commit

--- Lectures/Lecture2.py
+++ Lectures/Lecture2.py
@@ -6,3 +6,13 @@ end = None                                                # (4)
 df = yfinance.download(tic, start, end, ignore_tz=True)   # (5)
 print(df)                                                 # (6)
 df.to_csv('qan_stk_prc.csv')                              # (7)
+
+x = 1
+print(type(x))          # <class 'int'>
+
+xstr = '1'
+print(type(xstr))       # <class 'str'>
+
+test = x == xstr        # --> False
+print(test)
+print(type(test))       # <class 'bool'>
